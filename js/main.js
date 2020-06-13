3b8afb28a9f0458340605daae70d2c810e18ed9a
manuelp1345 <manuelp1345@gmail.com>
2020-06-13 00:54:35 -0400

    Animate Promos

--- js/main.js
+++ js/main.js
@@ -6,24 +6,35 @@ $(Document).ready(function(){
             e.preventDefault();
             if(ocultar==0)
             {
+            $(".thumb h2").html('<i class="fas fa-angle-double-down">');
+            $(".thumb h2").css({
+                transition: "all .3s linear",
+                left: "1.5rem",
+            })
+
             $(".promo").animate({
             right: "0rem"
             },500);
-/*             $(".texto-promo").css({
+            $(".texto-promo").css({
                 right: "-25rem"
-                },1); */
+                },1);
             $(".texto-promo").animate({
                 opacity: 1,
-               right: "0rem"
+                right: "0rem"
                 },600);
             ocultar++;
         }else{
+            $(".thumb h2").html('<i class="fas fa-angle-double-up"> Promos');
+            $(".thumb h2").css({
+                transition: "all .3s linear",
+                left: "-5.5rem",
+            })
             $(".promo").animate({
                 right: "-25rem"
                 },500);
                 $(".texto-promo").animate({
                     opacity: 0,
-                   right: "-25rem"
+                    right: "-25rem"
                     },1000);
                 ocultar--;
         }
